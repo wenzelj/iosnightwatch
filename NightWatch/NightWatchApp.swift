d322e5c5d335b034f67adeb2c226c1b735ff3b18
Wenzel Joubert <wenzel.joubert@gmail.com>
2021-03-28 14:28:51 +0800

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by wenzel Joubert on 28/3/21.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
